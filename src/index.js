2afb1af7cc8fcec9d554ec04739058b630b9d312
Jaime Flores <jaimelopesflores@gmail.com>
2018-11-16 19:35:29 -0200

    adding local storage persistence

--- src/index.js
+++ src/index.js
@@ -2,17 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from 'reducer';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const persistConfig = {
+  key: 'auto1:code-challenge',
+  storage: storage,
+  blacklist: ['app']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(persistedReducer, applyMiddleware(thunkMiddleware));
+const persistor = persistStore(store)
 
 ReactDOM.render(
   <Provider store={ store }>
-    <App />
+    <PersistGate loading={ null } persistor={ persistor }>
+      <App />
+    </PersistGate>
   </Provider>
   , document.getElementById('root'));
 
