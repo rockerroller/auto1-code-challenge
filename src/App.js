fce71bae63f31d4e00f957c432553e6113169080
Jaime Flores <jaimelopesflores@gmail.com>
2018-11-16 17:49:29 -0200

    adding redux to the solution

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from 'reducer';
 import routes from 'routes';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.scss';
 
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
 class App extends Component {
   render() {
     return (
-      <Router history={ createBrowserHistory() }>
-        <div className="app">
-          <Header />
-          <main>
-            <Switch>
-              {
-                routes.map((route, i) => (
-                  <Route exact={ route.exact } path={ route.path } component={ route.component } key={ i } />
-                ))
-              }
-            </Switch>
-          </main>
-          <Footer/>
-        </div>
-      </Router>
+      <Provider store={ store }>
+        <Router history={ createBrowserHistory() }>
+          <div className="app">
+            <Header />
+            <main>
+              <Switch>
+                {
+                  routes.map((route, i) => (
+                    <Route exact={ route.exact } path={ route.path } component={ route.component } key={ i } />
+                  ))
+                }
+              </Switch>
+            </main>
+            <Footer/>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ src/reducer/app.js
@@ -0,0 +1,5 @@
+import stateShape from './stateShape.json';
+
+export default function app(state = stateShape, action) {
+  return state;
+}
