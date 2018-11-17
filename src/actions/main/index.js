1661eae85a13e91fdbfc172df622e6c66e3edfe3
Jaime Flores <jaimelopesflores@gmail.com>
2018-11-17 00:36:12 -0200

    adding some new reducers

--- src/actions/main/index.js
+++ src/actions/main/index.js
@@ -2,6 +2,8 @@ export const STORE_COLORS = 'STORE_COLORS';
 export const STORE_MANUFACTURERS = 'STORE_MANUFACTURERS';
 export const SET_FILTER_COLOR = 'SET_FILTER_COLOR';
 export const SET_FILTER_MANUFACTURER = 'SET_FILTER_MANUFACTURER';
+export const SET_SORT_ORDER_BY_MILEAGE = 'SET_SORT_ORDER_BY_MILEAGE';
+export const SET_PAGE = 'SET_PAGE';
 
 export const storeColors = (value) => ({
   type: STORE_COLORS,
@@ -20,5 +22,15 @@ export const setFilterColor = (value) => ({
 
 export const setFilterManufacturer = (value) => ({
   type: SET_FILTER_MANUFACTURER,
+  value
+});
+
+export const setSortOrderByMileage = (value) => ({
+  type: SET_SORT_ORDER_BY_MILEAGE,
+  value
+});
+
+export const setPage = (value) => ({
+  type: SET_PAGE,
   value
 });
--- src/reducer/main/index.js
+++ src/reducer/main/index.js
@@ -4,7 +4,9 @@ import {
   STORE_COLORS,
   STORE_MANUFACTURERS,
   SET_FILTER_COLOR,
-  SET_FILTER_MANUFACTURER
+  SET_FILTER_MANUFACTURER,
+  SET_SORT_ORDER_BY_MILEAGE,
+  SET_PAGE
 } from 'actions/main';
 
 export default function app(state = initialState, action) {
@@ -21,6 +23,12 @@ export default function app(state = initialState, action) {
       return newState;
     case SET_FILTER_MANUFACTURER:
       newState.filter.manufacturer = action.value;
+      return newState;
+    case SET_SORT_ORDER_BY_MILEAGE:
+      newState.sort = action.value;
+      return newState;
+    case SET_PAGE:
+      newState.page = action.value;
       return newState;
     default: return state;
   }
