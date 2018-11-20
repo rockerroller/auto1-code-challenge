9f01d52c3d4fa79d3a12517150d04b7471b45699
Jaime Flores <jaimelopesflores@gmail.com>
2018-11-19 22:01:15 -0200

    removing some warnings

--- src/components/LabelledSelect/index.js
+++ src/components/LabelledSelect/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Select from 'components/Select';
